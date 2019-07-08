a6c1f4b1cd9ece5d9bf4fae5499c96e83aef2ab3
Steven <stevenanthonyzanga@gmail.com>
2019-07-07 23:47:49 -0400

    added finding profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -115,5 +115,48 @@ router.post('/', [ auth, [
   }
 );
 
+/** @route get api/profile
+ *  @desc get all profiles
+ *  @access public 
+ */
+
+ router.get('/', async (req,res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+ })
+
+ /** @route get api/profile/user/:user_id
+ *  @desc get profile by user id
+ *  @access public 
+ */
+
+router.get('/user/:user_id', async (req,res) => {
+  try {
+      const profile = await Profile.findOne({
+        user: req.params.user_id
+      }).populate('user', ['name', 'avatar']);
+
+      if(!profile) return res.status(400).json({
+        msg: 'profile not found'
+      });
+      res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind == 'ObjectId'){
+      return res.status(400).json({
+        msg: 'profile not found'
+      });
+    }
+    res.status(500).send('server error');
+  }
+})
+
 
 module.exports = router;
